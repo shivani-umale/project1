e9129f90258d80f2936e820e77f241ba882047b1
shivani-umale <73459522+shivani-umale@users.noreply.github.com>
2020-10-30 15:24:19 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
 		
-		int year=2004
+		int year=2004;
 		System.out.println("the year is leap  "+year);
 		
 		
