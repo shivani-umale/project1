adb4cc6c106cdf2e2a993e7882f789b2d11fcc58
shivani-umale <73459522+shivani-umale@users.noreply.github.com>
2020-10-30 15:20:19 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -4,20 +4,12 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class LeapYear {
-     public static boolean isLeap(int yr) {
-    	 Calendar cs=Calendar.getInstance();
-    	 cs.set(Calendar.YEAR,yr);
-    	 int no=cs.getActualMaximum(Calendar.DAY_OF_YEAR);
-    	 if(no>365)
-    		 return true;
-    	 return false;
-    	 
-     }
+     
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		
 		int year=2004
-		System.out.println("the year is leap  "+isLeap(year));
+		System.out.println("the year is leap  "+year);
 		
 		
 		
